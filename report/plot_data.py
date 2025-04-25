e504f75895ddd5d9b60174dd19607ef4d5d039c9
Kristian Gøystdal <krisgoy100@gmail.com>
2025-04-25 12:01:17 +0200

    Add initial report files including LaTeX document, plots, and auxiliary files

---
+++ report/plot_data.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+times_cmb = [42.42, 32.50, 21.84, 11.31, 4.06]
+EDP = [7601.49, 5143.95, 1315.92, 443.00, 73.74]
+Energy = [179.20, 158.28, 60.25, 39.17, 18.16]
+
+
